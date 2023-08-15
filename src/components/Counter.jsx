4ef0faba187f8f7ff428777ad001b6daecd09919
Albert <106256513+Alba2907@users.noreply.github.com>
2023-08-15 20:44:25 +0300

    start app

---
+++ src/components/Counter.jsx
@@ -0,0 +1,28 @@
+// import React, {useState} from 'react';
+//
+// const Counter = () => {
+//
+//     const [count, setCount] = useState(0)
+//
+//     const Increment = () => {
+//         console.log(count)
+//         setCount(count + 1) // сет лайка отвечает за состояние
+//
+//     }
+//
+//     const Decrement = () => {
+//         console.log(count)
+//         setCount(count - 1)
+//     }
+//
+//     return (
+//         <div>
+//             <h1>{count}</h1>
+//             <button onClick={Increment}>Increment</button>
+//             <button onClick={Decrement}>Decrement</button>
+//         </div>
+//
+//     );
+// };
+//
+// export default Counter;
